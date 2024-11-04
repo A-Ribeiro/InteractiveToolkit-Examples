718d593cd4ebb40b0299eb8fdf2e0095c93510b5
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2024-11-04 15:00:18 -0300

    mac free memory estimation

--- src/system-available-memory/main.cpp
+++ src/system-available-memory/main.cpp
@@ -58,7 +58,7 @@ namespace ITKCommon
 			vm_size_t used_memory = (static_cast<uint64_t>(vm_stats.active_count) + 
                            static_cast<uint64_t>(vm_stats.inactive_count) +
                            static_cast<uint64_t>(vm_stats.wire_count)) * sysconf(_SC_PAGESIZE);;
-			return total_ram() - (uint64_t)free_memory;
+			return total_ram() - (uint64_t)used_memory;
 #elif defined(_WIN32)
 			MEMORYSTATUSEX memStatus;
 			memStatus.dwLength = sizeof(memStatus);
