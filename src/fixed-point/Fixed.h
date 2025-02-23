77c82eb29e5375a78468f6bc851c86a7c1db48bd
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2025-02-22 21:18:16 -0300

    improved fixed point math

--- src/fixed-point/Fixed.h
+++ src/fixed-point/Fixed.h
@@ -3,8 +3,9 @@
 #include <InteractiveToolkit/InteractiveToolkit.h>
 #include <cstdint>
 
-template<int frac_bits>
-class fixed32_t {
+template <int frac_bits>
+class fixed32_t
+{
 private:
     int32_t value;
 
@@ -13,67 +14,130 @@ private:
     static const int32_t FACTOR = 1 << FRACTIONAL_BITS;
 
 public:
-    fixed32_t(int32_t integerPart, int32_t fractionalPart) {
-        value = (integerPart << FRACTIONAL_BITS) | (fractionalPart & FRACTIONAL_MASK);
+    static const uint32_t INT_BITS = 32 - FRACTIONAL_BITS;
+    static const int32_t INT_RANGE_MIN = -(1 << (INT_BITS - 1));
+    static const int32_t INT_RANGE_MAX = (1 << (INT_BITS - 1)) - 1;
+
+    static const uint32_t FRAC_BITS = FRACTIONAL_BITS;
+    static const int32_t FRAC_RANGE_MIN = 0;
+    static const int32_t FRAC_RANGE_MAX = (1 << FRAC_BITS) - 1;
+
+    fixed32_t(int32_t integerPart, int32_t fractionalPart)
+    {
+        //value = (integerPart << FRACTIONAL_BITS) | (fractionalPart & FRACTIONAL_MASK);
+        value = (integerPart << FRACTIONAL_BITS) + fractionalPart;
     }
 
     fixed32_t(int32_t rawValue) : value(rawValue) {}
 
-    fixed32_t operator+(const fixed32_t& other) const {
+    fixed32_t operator+(const fixed32_t &other) const
+    {
         return fixed32_t(value + other.value);
     }
 
-    fixed32_t operator-(const fixed32_t& other) const {
+    fixed32_t operator-(const fixed32_t &other) const
+    {
         return fixed32_t(value - other.value);
     }
 
-    fixed32_t operator-() const {
+    fixed32_t operator-() const
+    {
         return fixed32_t(-value);
     }
 
-    fixed32_t operator*(const fixed32_t& other) const {
+    fixed32_t operator*(const fixed32_t &other) const
+    {
         int64_t result = static_cast<int64_t>(value) * other.value;
         return fixed32_t(static_cast<int32_t>(result >> FRACTIONAL_BITS));
     }
 
-    fixed32_t operator/(const fixed32_t& other) const {
+    fixed32_t operator/(const fixed32_t &other) const
+    {
         int64_t result = (static_cast<int64_t>(value) << FRACTIONAL_BITS) / other.value;
         return fixed32_t(static_cast<int32_t>(result));
     }
 
-    double toDouble() const {
+    double toDouble() const
+    {
         bool sign_negative = (value & 0x80000000) != 0;
-        uint32_t uvalue = (sign_negative)?-value:value;
+        uint32_t uvalue = (sign_negative) ? -value : value;
         uint32_t integerPart = uvalue >> FRACTIONAL_BITS;
         uint32_t fractionalPart = uvalue & FRACTIONAL_MASK;
         double number = (double)integerPart + (double)fractionalPart / (double)FACTOR;
-        return (sign_negative)?-number:number;
+        return (sign_negative) ? -number : number;
     }
 
-    double toFloat() const {
+    double toFloat() const
+    {
         bool sign_negative = (value & 0x80000000) != 0;
-        uint32_t uvalue = (sign_negative)?-value:value;
+        uint32_t uvalue = (sign_negative) ? -value : value;
         uint32_t integerPart = uvalue >> FRACTIONAL_BITS;
         uint32_t fractionalPart = uvalue & FRACTIONAL_MASK;
         float number = (float)integerPart + (float)fractionalPart / (float)FACTOR;
-        return (sign_negative)?-number:number;
+        return (sign_negative) ? -number : number;
     }
 
-    std::string toString() const {
-        return ITKCommon::PrintfToStdString("%f",toDouble());
-    }
-    
-    int32_t integer_part() const {
-        return (value >> FRACTIONAL_BITS);
+    std::string toString() const
+    {
+        return ITKCommon::PrintfToStdString("%f", toDouble());
     }
 
-    int32_t fract_part() const {
-        return (value & 0x80000000) ? (-(-value >> FRACTIONAL_MASK)) : (value >> FRACTIONAL_MASK);
+    int32_t sign() const
+    {
+        static constexpr int shift_to_get_sign_minus_one = 30;
+        static constexpr uint32_t first_bit_zero = UINT32_C(0xfffffffe);
+
+        return ((value >> shift_to_get_sign_minus_one) & first_bit_zero) + 1;
+    }
+
+    bool isNegative() const {
+        return (value & 0x80000000) != 0;
+    }
+
+    int32_t signed_integer_part() const
+    {
+        return (value & 0x80000000) ? (-(-value >> FRACTIONAL_BITS)) : (value >> FRACTIONAL_BITS);
+    }
+
+    int32_t integer_part() const
+    {
+        return (value & 0x80000000) ? ((-value >> FRACTIONAL_BITS)) : (value >> FRACTIONAL_BITS);
+    }
+
+    int32_t signed_fract_part() const
+    {
+        //return (value & 0x80000000) ? ((int32_t)FRACTIONAL_MASK - (-value & FRACTIONAL_MASK)) : (value & FRACTIONAL_MASK);
+        return (value & 0x80000000) ? (-(-value & FRACTIONAL_MASK)) : (value & FRACTIONAL_MASK);
+    }
+
+    int32_t fract_part() const
+    {
+        //return (value & 0x80000000) ? ((int32_t)FRACTIONAL_MASK - (-value & FRACTIONAL_MASK)) : (value & FRACTIONAL_MASK);
+        return (value & 0x80000000) ? ((-value & FRACTIONAL_MASK)) : (value & FRACTIONAL_MASK);
+    }
+
+    static constexpr fixed32_t fromFloat(float v) noexcept
+    {
+        bool sign_negative = v < 0;
+        v = (sign_negative) ? -v : v;
+        uint32_t integerPart = (uint32_t)v;
+        uint32_t fractionalPart = (uint32_t)((v - (float)integerPart) * (float)FACTOR);
+        return (sign_negative) ? -fixed32_t(integerPart, fractionalPart) : fixed32_t(integerPart, fractionalPart);
+    }
+
+    static constexpr fixed32_t fromDouble(double v) noexcept
+    {
+        bool sign_negative = v < 0;
+        v = (sign_negative) ? -v : v;
+        uint32_t integerPart = (uint32_t)v;
+        uint32_t fractionalPart = (uint32_t)((v - (double)integerPart) * (double)FACTOR);
+        return (sign_negative) ? -fixed32_t(integerPart, fractionalPart) : fixed32_t(integerPart, fractionalPart);
     }
 };
 
-template<int frac_bits>
-class ufixed32_t {
+template <int frac_bits>
+class ufixed32_t
+{
 private:
     uint32_t value;
 
@@ -82,60 +146,90 @@ private:
     static const uint32_t FACTOR = 1 << FRACTIONAL_BITS;
 
 public:
-    ufixed32_t(uint32_t integerPart, uint32_t fractionalPart) {
+    static const uint32_t INT_BITS = 32 - FRACTIONAL_BITS;
+    static const int32_t INT_RANGE_MIN = 0;
+    static const int32_t INT_RANGE_MAX = (1 << INT_BITS) - 1;
+
+    static const uint32_t FRAC_BITS = FRACTIONAL_BITS;
+    static const int32_t FRAC_RANGE_MIN = 0;
+    static const int32_t FRAC_RANGE_MAX = (1 << FRAC_BITS) - 1;
+
+    ufixed32_t(uint32_t integerPart, uint32_t fractionalPart)
+    {
         value = (integerPart << FRACTIONAL_BITS) | (fractionalPart & FRACTIONAL_MASK);
     }
 
     ufixed32_t(uint32_t rawValue) : value(rawValue) {}
 
-    ufixed32_t operator+(const ufixed32_t& other) const {
+    ufixed32_t operator+(const ufixed32_t &other) const
+    {
         return ufixed32_t(value + other.value);
     }
 
-    ufixed32_t operator-(const ufixed32_t& other) const {
+    ufixed32_t operator-(const ufixed32_t &other) const
+    {
         return ufixed32_t(value - other.value);
     }
 
-    ufixed32_t operator-() const {
+    ufixed32_t operator-() const
+    {
         return ufixed32_t(-value);
     }
 
-    ufixed32_t operator*(const ufixed32_t& other) const {
+    ufixed32_t operator*(const ufixed32_t &other) const
+    {
         int64_t result = static_cast<int64_t>(value) * other.value;
         return ufixed32_t(static_cast<uint32_t>(result >> FRACTIONAL_BITS));
     }
 
-    ufixed32_t operator/(const ufixed32_t& other) const {
+    ufixed32_t operator/(const ufixed32_t &other) const
+    {
         int64_t result = (static_cast<int64_t>(value) << FRACTIONAL_BITS) / other.value;
         return ufixed32_t(static_cast<uint32_t>(result));
     }
 
-    double toDouble() const {
+    double toDouble() const
+    {
         uint32_t integerPart = value >> FRACTIONAL_BITS;
         uint32_t fractionalPart = value & FRACTIONAL_MASK;
         double number = (double)integerPart + (double)fractionalPart / (double)FACTOR;
         return number;
     }
 
-    double toFloat() const {
+    double toFloat() const
+    {
         uint32_t integerPart = value >> FRACTIONAL_BITS;
         uint32_t fractionalPart = value & FRACTIONAL_MASK;
         float number = (float)integerPart + (float)fractionalPart / (float)FACTOR;
         return number;
     }
 
-    std::string toString() const {
-        return ITKCommon::PrintfToStdString("%f",toDouble());
+    std::string toString() const
+    {
+        return ITKCommon::PrintfToStdString("%f", toDouble());
     }
 
-
-    uint32_t integer_part() const {
+    uint32_t integer_part() const
+    {
         return value >> FRACTIONAL_BITS;
     }
 
-    uint32_t fract_part() const {
+    uint32_t fract_part() const
+    {
         return value & FRACTIONAL_MASK;
     }
 
-};
+    static constexpr ufixed32_t fromFloat(float v) noexcept
+    {
+        uint32_t integerPart = (uint32_t)v;
+        uint32_t fractionalPart = (uint32_t)((v - (float)integerPart) * (float)FACTOR);
+        return ufixed32_t(integerPart, fractionalPart);
+    }
 
+    static constexpr ufixed32_t fromDouble(double v) noexcept
+    {
+        uint32_t integerPart = (uint32_t)v;
+        uint32_t fractionalPart = (uint32_t)((v - (double)integerPart) * (double)FACTOR);
+        return ufixed32_t(integerPart, fractionalPart);
+    }
+};
--- src/fixed-point/main.cpp
+++ src/fixed-point/main.cpp
@@ -4,23 +4,76 @@ int main(int argc, char *argv[])
 {
 	ITKCommon::Path::setWorkingPath(ITKCommon::Path::getExecutablePath(argv[0]));
 
-	using fixed = ufixed32_t<16>;
-
-	auto a = fixed(0, 0);
-	auto b = fixed(100, 0);
-	auto delta = b - a;
-
-	int n_div = 10;
-
-	for (int i = 0; i <= n_div; i++)
 	{
-		// method 1: not so precise
-		// auto lrp = fixed(i, 0) / fixed(n_div, 0);
-		// auto result = a + delta * lrp;
-		// method 2: better precision for integers
-		auto result = a + ( delta * fixed(i, 0) ) / fixed(n_div, 0);
+		printf(
+			" ----------\n"
+			"|  TEST A  |\n"
+			" ----------\n");
 
-		printf("%i => %f\n", i, result.toFloat());
+		// 16bits to the integer part -- range: [0,65536]
+		// 16bits to the decimal part -- range: [0,65536]
+		using fixed = ufixed32_t<16>;
+
+		printf("ufixed32_t<%i>:\n", fixed::FRAC_BITS);
+		printf("  int_bits:%i [%i -> %i]\n", fixed::INT_BITS, fixed::INT_RANGE_MIN, fixed::INT_RANGE_MAX);
+		printf("  frac_bits:%i [%i -> %i]\n", fixed::FRAC_BITS, fixed::FRAC_RANGE_MIN, fixed::FRAC_RANGE_MAX);
+
+		auto a = fixed(0, 0);
+		auto b = fixed(100, 0);
+		auto delta = b - a;
+
+		int n_div = 10;
+
+		for (int i = 0; i <= n_div; i++)
+		{
+			// method 1: not so precise
+			// auto lrp = fixed(i, 0) / fixed(n_div, 0);
+			// auto result = a + delta * lrp;
+			// method 2: better precision for integers
+			auto result = a + (delta * fixed(i, 0)) / fixed(n_div, 0);
+
+			printf("%i => %f\n", i, result.toFloat());
+		}
+	}
+
+	{
+		printf(
+			" ----------\n"
+			"|  TEST B  |\n"
+			" ----------\n");
+
+		// 11bits to the integer part -- range: [-1024,1023]
+		// 21bits to the decimal part -- range: [0,2097151]
+		using fixed = fixed32_t<21>;
+
+		printf("fixed32_t<%i>:\n", fixed::FRAC_BITS);
+		printf("  int_bits:%i [%i -> %i]\n", fixed::INT_BITS, fixed::INT_RANGE_MIN, fixed::INT_RANGE_MAX);
+		printf("  frac_bits:%i [%i -> %i]\n", fixed::FRAC_BITS, fixed::FRAC_RANGE_MIN, fixed::FRAC_RANGE_MAX);
+
+		auto a = fixed(-255, 0);
+		auto b = fixed(0, 0);
+		const auto lrp = fixed::fromFloat(0.35f);
+
+		printf("a => %f\n", a.toFloat());
+		printf("b => %f\n", b.toFloat());
+		printf("b - a => %f\n", (b - a).toFloat());
+		printf("lrp => %f\n", lrp.toFloat());
+
+		auto result = a + (b - a) * lrp;
+		printf("result => %f\n", result.toFloat());
+
+		// round result
+		const auto _0_5 = fixed::fromFloat(0.5f);
+		result = (result.isNegative()) ? (result - _0_5) : (result + _0_5);
+		printf("result => %f\n", result.toFloat());
+
+		printf("result.integer_part() => %i\n", result.signed_integer_part());
+		printf("result.fract_part() => %i -> %f\n", result.signed_fract_part(), (float)result.signed_fract_part() / (fixed::FRAC_RANGE_MAX + 1));
+
+		// auto new_from_result = (result.isNegative()) ? -fixed(result.integer_part(), result.fract_part()) : fixed(result.integer_part(), result.fract_part());
+		auto new_from_result = fixed(result.signed_integer_part(), result.signed_fract_part());
+		
+		printf("new_from_result => %f\n", new_from_result.toFloat());
 	}
 
 	return 0;
